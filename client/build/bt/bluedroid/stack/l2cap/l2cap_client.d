a168a2534cfc9a654029427f28c389409da13128
Anderson Rodrigues <makiander.abr@gmail.com>
2019-11-13 20:45:52 -0300

    removed build folder and updated .gitignore

--- client/build/bt/bluedroid/stack/l2cap/l2cap_client.d
+++
@@ -1,3 +0,0 @@
-bluedroid/stack/l2cap/l2cap_client.o: \
- /home/anderson/esp/esp-mdf/esp-idf/components/bt/bluedroid/stack/l2cap/l2cap_client.c
-/home/anderson/esp/esp-mdf/esp-idf/components/bt/bluedroid/stack/l2cap/l2cap_client.c:
--- client/build/libsodium/libsodium/src/libsodium/crypto_shorthash/siphash24/shorthash_siphashx24.d
+++
@@ -1,9 +0,0 @@
-libsodium/src/libsodium/crypto_shorthash/siphash24/shorthash_siphashx24.o: \
- /home/anderson/esp/esp-mdf/esp-idf/components/libsodium/libsodium/src/libsodium/crypto_shorthash/siphash24/shorthash_siphashx24.c \
- /home/anderson/esp/esp-mdf/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/crypto_shorthash_siphash24.h \
- /home/anderson/esp/esp-mdf/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/export.h
-
-/home/anderson/esp/esp-mdf/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/crypto_shorthash_siphash24.h:
-
-/home/anderson/esp/esp-mdf/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/export.h:
-/home/anderson/esp/esp-mdf/esp-idf/components/libsodium/libsodium/src/libsodium/crypto_shorthash/siphash24/shorthash_siphashx24.c:
